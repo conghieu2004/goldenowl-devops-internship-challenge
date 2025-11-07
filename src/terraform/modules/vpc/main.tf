cde5b7e2baf9fffea1fdf49abf463f7b0dc69e70
unknown <hoconghieu2k4@gmail.com>
2025-11-07 21:55:24 +0700

    feat: add Dockerfile and fix vpc module

--- src/terraform/modules/vpc/main.tf
+++ src/terraform/modules/vpc/main.tf
@@ -2,12 +2,16 @@ resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "${var.project_name}-vpc" }
+  tags = { 
+    Name = "${var.project_name}-vpc" 
+  }
 }
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-  tags = { Name = "${var.project_name}-igw" }
+  tags = { 
+    Name = "${var.project_name}-igw" 
+  }
 }
 
 resource "aws_eip" "nat" {
@@ -18,7 +22,9 @@ resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
   depends_on    = [aws_internet_gateway.this]
-  tags = { Name = "${var.project_name}-nat" }
+  tags = { 
+    Name = "${var.project_name}-nat" 
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -27,7 +33,9 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = element(var.azs, count.index)
   map_public_ip_on_launch = true
-  tags = { Name = "${var.project_name}-public-${count.index}" }
+  tags = { 
+    Name = "${var.project_name}-public-${count.index}" 
+  }
 }
 
 resource "aws_subnet" "private" {
@@ -35,5 +43,56 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = element(var.azs, count.index)
-  tags = { Name = "${var.project_name}-private-${count.index}" }
+  tags = { 
+    Name = "${var.project_name}-private-${count.index}" 
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# Route table cho private subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+# Routes
+# Public route tới Internet Gateway
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+# Private route tới NAT Gateway
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+
+#################################
+# Route Table Associations
+#################################
+# Gán public subnets vào public route table
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Gán private subnets vào private route table
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
--- src/terraform/modules/vpc/outputs.tf
+++ src/terraform/modules/vpc/outputs.tf
@@ -9,3 +9,11 @@ output "public_subnets" {
 output "private_subnets" {
   value = aws_subnet.private[*].id
 }
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
